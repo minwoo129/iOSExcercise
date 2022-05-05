7d65351ea401b4474fd3adf313a4d832002a6df6
minwoo129 <key0129mw@naver.com>
2022-05-05 12:55:30 +0900

    web: 버튼별 액션 추가

--- Web/Web/ViewController.swift
+++ Web/Web/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var txtUrl: UITextField!
+    @IBOutlet var myWebView: WKWebView!
+    @IBOutlet var myActivityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnGotoUrl(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnGotoSite1(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnGotoSite2(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+    }
+    
+    // toolbar
+    
+    @IBAction func btnStop(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnGoFoward(_ sender: UIBarButtonItem) {
+    }
 }
 
