6087f6fcc6728978aeef0aef000d0181071f2bd7
minwoo129 <key0129mw@naver.com>
2022-05-05 13:07:02 +0900

    Web: ActivityIndicator 추가

--- Web/Web/ViewController.swift
+++ Web/Web/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
     @IBOutlet var txtUrl: UITextField!
     @IBOutlet var myWebView: WKWebView!
     @IBOutlet var myActivityIndicator: UIActivityIndicatorView!
@@ -22,7 +22,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myWebView.navigationDelegate = self
         loadWebPage("http://2sam.net")
+    }
+    
+    // 웹뷰 로딩 중일 때
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        myActivityIndicator.startAnimating()
+        myActivityIndicator.isHidden = false
+    }
+    
+    // 웹뷰 로딩이 끝났을 때
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        myActivityIndicator.stopAnimating()
+        myActivityIndicator.isHidden = true
+    }
+    
+    // 웹뷰 로딩이 실패했을 때
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        myActivityIndicator.stopAnimating()
+        myActivityIndicator.isHidden = true
     }
 
     @IBAction func btnGotoUrl(_ sender: UIButton) {
