6f0ef7c67c6f7d258fa450da82f58ce260984714
minwoo129 <key0129mw@naver.com>
2022-09-12 14:33:49 +0900

    map 프로젝트 재생성

---
+++ Map1/Map1/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Map1
+//
+//  Created by 류민우 on 2022/09/12.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet var myMap: MKMapView!
+    @IBOutlet var lblLocationInfo1: UILabel!
+    @IBOutlet var lblLocationInfo2: UILabel!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lblLocationInfo1.text = ""
+        lblLocationInfo2.text = ""
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        myMap.showsUserLocation = true
+    }
+
+    @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
+    }
+    
+}
+
