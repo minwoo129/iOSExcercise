4d87acb4860736613f236767b2996355a465d09f
minwoo129 <key0129mw@naver.com>
2022-09-12 14:47:26 +0900

    Map 위치정보 추출해 텍스트로 표시하기

--- Map1/Map1/ViewController.swift
+++ Map1/Map1/ViewController.swift
@@ -26,6 +26,34 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
         myMap.showsUserLocation = true
     }
+    
+    func goLocation(latitudeValue:CLLocationDegrees, longitudeValue:CLLocationDegrees, delta span :Double) {
+        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+        myMap.setRegion(pRegion, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let pLocation = locations.last
+        goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
+        CLGeocoder().reverseGeocodeLocation(pLocation!, completionHandler: {(placeMarks, error) -> Void in
+            let pm = placeMarks!.first
+            let country = pm!.country
+            var address:String = country!
+            if pm!.locality != nil {
+                address += " "
+                address += pm!.locality!
+            }
+            if pm!.thoroughfare != nil {
+                address += " "
+                address += pm!.thoroughfare!
+            }
+            self.lblLocationInfo1.text = "현재 위치"
+            self.lblLocationInfo2.text = address
+        })
+        locationManager.stopUpdatingLocation()
+    }
 
     @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
     }
